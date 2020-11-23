7a55f9cbc2e462c08454552d4509d4a3b8e6d006
pierce.faraone <pierce.faraone@gmail.com>
2020-11-23 21:49:07 +0800

    added links to external personal sites and svg icons for header

---
+++ src/links.js
@@ -0,0 +1,4 @@
+export const github = "https://github.com/pfaraone"
+export const linkedin = "https://www.linkedin.com/in/pierce-faraone/"
+export const dropboxresume =
+  "https://www.dropbox.com/s/p72uw2ilovdf17s/Pierce_Faraone_Resume.pdf?dl=0"
